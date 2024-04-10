81e7ba160cab49e7bec68d0c680bf4c8dd327a5c
Andrew Sansum <sansumandrew@gmail.com>
2024-04-10 19:02:00 +0100

    Display blog details in page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,25 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+
   return (
     <div className="blog-details">
-      <h2>Blog Details</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <h3>{blog.author}</h3>
+          <p>{blog.body}</p>
+        </article>
+      )}
     </div>
   );
 };
