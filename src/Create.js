f15c93e9129705a8adb64ee1be099b0327baf436
Andrew Sansum <sansumandrew@gmail.com>
2024-04-10 19:08:07 +0100

    Add form to blog creation page

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,17 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Title:</label>
+        <input type="text" required />
+        <label>Body:</label>
+        <textarea required></textarea>
+        <label>Author:</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+      </form>
     </div>
   );
 };
