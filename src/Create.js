a5a4c08bfba453157a0401a79fe89012488721ea
Andrew Sansum <sansumandrew@gmail.com>
2024-04-10 19:20:59 +0100

    Add state to input fields

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
       <form>
         <label>Title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Author:</label>
-        <select>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
