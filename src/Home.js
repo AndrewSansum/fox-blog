f2a3fa5edb80042a9d95ebf42b4da08bca3a46fc
Andrew Sansum <sansumandrew@gmail.com>
2024-04-10 14:33:09 +0100

    Add button to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 const Home = () => {
+  const handleClick = () => {};
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
